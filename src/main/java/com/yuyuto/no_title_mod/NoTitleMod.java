d3079a14a8b08be418ee37623f288d0462c6786c
Yuyuto <yuyuto0514@gmail.com>
2026-07-19 08:21:32 +0900

    debugging 9

--- src/main/java/com/yuyuto/no_title_mod/NoTitleMod.java
+++ src/main/java/com/yuyuto/no_title_mod/NoTitleMod.java
@@ -1,35 +1,38 @@
 package com.yuyuto.no_title_mod;
 
+import com.mojang.logging.LogUtils;
 import com.yuyuto.no_title_mod.event.ModCraftingEvent;
 import com.yuyuto.no_title_mod.registry.*;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
 import software.bernie.geckolib.GeckoLib;
 
 @Mod(NoTitleMod.MODID)
 public class NoTitleMod {
 
     public static final String MODID = "notitlemod";
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public NoTitleMod() {
         GeckoLib.initialize();
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModItems.register(modEventBus);
-        System.out.println("[YUYUTO_TECHNOLOGIES][DEBUG] Item Loaded successful");
+        NoTitleMod.LOGGER.info("[YUYUTO_TECHNOLOGIES][INFO] Item Loaded successful");
         ModBlocks.register(modEventBus);
-        System.out.println("[YUYUTO_TECHNOLOGIES][DEBUG] Block Loaded successful");
+        NoTitleMod.LOGGER.info("[YUYUTO_TECHNOLOGIES][INFO] Block Loaded successful");
         ModBlockEntities.register(modEventBus);
-        System.out.println("[YUYUTO_TECHNOLOGIES][DEBUG] BlockEntity Loaded successful");
+        NoTitleMod.LOGGER.info("[YUYUTO_TECHNOLOGIES][INFO] BlockEntity Loaded successful");
         ModCreativeTabs.register(modEventBus);
-        System.out.println("[YUYUTO_TECHNOLOGIES][DEBUG] CreativeTab Loaded successful");
+        NoTitleMod.LOGGER.info("[YUYUTO_TECHNOLOGIES][INFO] CreativeTab Loaded successful");
         ModRecipeSerializers.register(modEventBus);
-        System.out.println("[YUYUTO_TECHNOLOGIES][DEBUG] RecipeSerializer Loaded successful");
+        NoTitleMod.LOGGER.info("[YUYUTO_TECHNOLOGIES][INFO] RecipeSerializer Loaded successful");
         ModRecipeTypes.register(modEventBus);
-        System.out.println("[YUYUTO_TECHNOLOGIES][DEBUG] RecipeType Loaded successful");
+        NoTitleMod.LOGGER.info("[YUYUTO_TECHNOLOGIES][INFO] RecipeType Loaded successful");
         MinecraftForge.EVENT_BUS.register(ModCraftingEvent.class);
-        System.out.println("[YUYUTO_TECHNOLOGIES][DEBUG] NoTitleMod(NTMod) Loaded successful");
+        NoTitleMod.LOGGER.info("[YUYUTO_TECHNOLOGIES][INFO] NoTitleMod(NTMod) Loaded successful");
     }
 
 }
